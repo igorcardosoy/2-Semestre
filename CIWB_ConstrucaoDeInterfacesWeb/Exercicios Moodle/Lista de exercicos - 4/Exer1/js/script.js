aef62d9f8a580e2a4c474a9a21aac13c93cae796
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-09-01 10:45:35 -0300

    Calculadora em js

--- CIWB_ConstrucaoDeInterfacesWeb/Exercicios Moodle/Lista de exercicos - 4/Exer1/js/script.js
+++ CIWB_ConstrucaoDeInterfacesWeb/Exercicios Moodle/Lista de exercicos - 4/Exer1/js/script.js
@@ -1,13 +1,116 @@
-let allButtons = document.getElementsByTagName("button");
+const allButtons = document.getElementsByTagName("button");
+const input = document.querySelector("input");
+
+let position = 0;
+let number = [];
+let op;
 
 for (let index = 0; index < allButtons.length; index++) {
     allButtons[index].addEventListener("click", () => {
-        console.log(allButtons[index].innerHTML);
+        
+        let keyPressed = allButtons[index].innerHTML
 
-        let keyPressed = allButtons[index].innerHTML;
+        if (!isNaN(keyPressed) || keyPressed == ".") {
+            input.value += keyPressed;
+        }
+        else
+        {
+            switch (keyPressed) {
+                case "←":
+                    input.value = input.value.slice(0, input.value.length - 1);
+                    break;
+                case "CE":
+                    input.value = null;
+                    break;
+                case "C":
+                    number[0] = null;
+                    number[1] = null;
+                    position = 0;
+                    operator = null;
+                    input.value = null;
+                    break;
+                case "/":
+                    number[position] = (input.value);
+                    input.value = null;
 
-        if (!isNaN(keyPressed)) {
-            console.log(parseInt(keyPressed))
+                    op = "/";
+
+                    if (position == 1) {
+                        position = 0;
+                        number[0] = parseFloat(number[0]) / parseFloat(number[1]);
+                        input.value = number[0];
+                    }
+                    position++;
+                    break;
+                case "*":
+                    number[position] = (input.value);
+                    input.value = null;
+
+                    op = "*";
+
+                    if (position == 1) {
+                        position = 0;
+                        number[0] = parseFloat(number[0]) * parseFloat(number[1]);
+                        input.value = number[0];
+                    }
+                    position++;
+                    break;
+                case "+":
+                    number[position] = (input.value);
+                    input.value = null;
+
+                    op = "+";
+
+                    if (position == 1) {
+                        position = 0;
+                        number[0] = parseFloat(number[0]) + parseFloat(number[1]);
+                        input.value = number[0];
+                    }
+                    position++;
+                    break;
+                case "-":
+                    number[position] = (input.value);
+                    input.value = null;
+
+                    op = "-";
+
+                    if (position == 1) {
+                        position = 0;
+                        number[0] = parseFloat(number[0]) - parseFloat(number[1]);
+                        input.value = number[0];
+                    }
+                    position++;
+                    break;
+                case "=":
+
+                    number[position] = parseFloat(input.value);
+                    position = 0;
+
+                    switch (op) {
+                        case "+":
+                            number[0] = parseFloat(number[0]) + parseFloat(number[1]);
+                            break;
+                        case "-":
+                            number[0] = parseFloat(number[0]) - parseFloat(number[1]);
+                            break;
+                        case "/":
+                            number[0] = parseFloat(number[0]) / parseFloat(number[1]);
+                            break;
+                        case "*":
+                            number[0] = parseFloat(number[0]) * parseFloat(number[1]);
+                            break;
+                        default:
+                        break;
+                    }
+
+                    position = 0;
+                    input.value = number[0];
+
+                    break;
+                default:
+                    console.log("Não encontrado!");
+                    break;
+            }
         }
 
     });
