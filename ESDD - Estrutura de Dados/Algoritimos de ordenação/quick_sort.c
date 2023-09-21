1d23a3c622c1f76311b15aafb4888ff9f0199915
Igor Notebook <igorfilipicardoso@gmail.com>
2023-09-21 19:21:18 -0300

    Outra implementação do quick

--- ESDD - Estrutura de Dados/Algoritimos de ordenação/quick_sort.c
+++ ESDD - Estrutura de Dados/Algoritimos de ordenação/quick_sort.c
@@ -164,3 +164,67 @@ void quickSort(int arr[], int left, int right)
 
         quickSort(arr, i, right);
 }
+
+void quicksort2(int x[], int lb, int ub){
+
+    int j = -1;
+
+    if(lb >= ub)
+        return;                     /* Vetor está classificado */
+
+    partition(x, lb, ub, &j);       /* Particiona os elementos do subvetor
+                                     * de modo que um dos elementos seja alocado
+                                     * em sua posição correta. A variável j é a
+                                     * posição onde este elemento foi alocada
+                                     */
+
+    quicksort2(x, lb, j-1);          /* Classifica de forma recursiva o subvetor
+                                     * entre a posição lb e j-1
+                                     */
+
+    quicksort2(x, j+1, ub);          /* Classifica de forma recursiva o subvetor
+                                     * entre a posição j+1 e ub
+                                     */
+
+}
+
+
+void partition(int x[], int lb, int ub, int *j){
+
+    int a, down, up, temp;
+    
+    /*
+     * a é o elemento cuja posição 
+     * final é procurada (pivô)
+     */
+    a = x[lb];
+
+    /*
+     * Inicialização dos ponteiros
+     */
+    up = ub;
+    down = lb;
+
+    while(down < up){
+        while(x[down] <= a && down < ub){
+            /*
+             * Sobe no vetor
+             */
+            down++;
+        }
+        while(x[up] > a){
+            /*
+             * Desce no vetor
+             */
+            up--;
+        }
+        if(down < up){
+            temp = x[down];
+            x[down] = x[up];
+            x[up] = temp;
+        }
+    }
+    x[lb] = x[up];
+    x[up] = a;
+    *j = up;
+}
