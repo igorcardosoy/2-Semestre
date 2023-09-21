f5c09da82774ab1d8c03dc312fd3faaf3c035906
Igor Cardoso <66184416+IgorFilipiCardoso@users.noreply.github.com>
2023-09-20 22:16:28 -0300

    Update insertValue.c

--- ESDD - Estrutura de Dados/Exercicios de Revisão/Exer2/insertValue.c
+++ ESDD - Estrutura de Dados/Exercicios de Revisão/Exer2/insertValue.c
@@ -5,31 +5,42 @@
 void loadArray(int arrayInt[], int size);
 void insertValue(int arrayInt[], int value, int size);
 void printArray(int arrayInt[], int size);
-void incressSize(int array[], int *size);
+void increaseSize(int array[], int *size);
 
 int main()
 {
-    int size = 10;
+    int size = 5, x;
     int *arrayInt, value;
     arrayInt = malloc(sizeof(int) * size);
+    bool stop = false;
 
     loadArray(arrayInt, size);
     printArray(arrayInt, size);
 
-    printf("\nDigite o valor a adicionar ao array: ");
-    scanf("%d", &value);
+    while (!stop)
+    {
+        printf("\nDigite o valor a adicionar ao array: ");
+        scanf("%d", &value);
 
-    incressSize(arrayInt, &size);
-    insertValue(arrayInt, value, size);
+        increaseSize(arrayInt, &size);
+        insertValue(arrayInt, value, size);
 
-    printArray(arrayInt, size);
+        printArray(arrayInt, size);
 
+        printf("\nParar? (1/0): ");
+        scanf("%d", &x);
+
+        stop = x == 1 ? true : false;
+
+    }
+    
     free(arrayInt);
+    
     return 0;
 }
-void incressSize(int array[], int *size){
-    *size += 1;
-    array = realloc(array, (*size)  * sizeof(int));
+void increaseSize(int array[], int *size){
+    *size+=1;
+    array = realloc(*array, sizeof(int) * (*size));
 }
 
 void loadArray(int arrayInt[], int size)
@@ -46,7 +57,7 @@ void insertValue(int arrayInt[], int value, int size)
 
     for (int i = 0; i < size && !isInserted; i++)
     {
-        if (arrayInt[i] > value)
+        if (arrayInt[i] >= value)
         {
             for (int j = size - 2; j >= i; j--)
             {
