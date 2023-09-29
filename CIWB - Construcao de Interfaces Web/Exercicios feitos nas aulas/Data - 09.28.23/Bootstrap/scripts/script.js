459c898883441f40c5050e1283f6e2703fd11e16
Igor Notebook <igorfilipicardoso@gmail.com>
2023-09-29 19:20:54 -0300

    Exercicoi de Bootstrap

---
+++ CIWB - Construcao de Interfaces Web/Exercicios feitos nas aulas/Data - 09.28.23/Bootstrap/scripts/script.js
@@ -0,0 +1,21 @@
+const buttonDarkMode = document.querySelector(".darkMode");
+const html = document.querySelector("html");
+const submiteButton = document.querySelector(".submit");
+let darkMode = true;
+
+buttonDarkMode.addEventListener("click", () => {
+
+    if (darkMode == true) {
+        html.removeAttribute("data-bs-theme");
+        html.setAttribute("data-bs-theme", "light");
+        buttonDarkMode.removeAttribute("class");
+        buttonDarkMode.setAttribute("class", "btn btn-light");
+        darkMode = false;
+    } else {
+        html.removeAttribute("data-bs-theme");
+        html.setAttribute("data-bs-theme", "dark");
+        buttonDarkMode.removeAttribute("class");
+        buttonDarkMode.setAttribute("class", "btn btn-dark");
+        darkMode = true;
+    }  
+})
